e2578b09cca1c217bcaa2178a024240e66101839
apolo <scoutardilla@gmail.com>
2018-01-16 09:20:52 +0100

    ejemplo dia 16/1/18

---
+++ Ejercicio02.java
@@ -0,0 +1,43 @@
+package p02;
+
+import java.util.Scanner;
+
+
+public class Ejercicio02 {
+
+	private static Scanner teclado;
+
+	public static void main(String[] args) {
+		
+		int n;
+		
+		
+		teclado = new Scanner(System.in);
+
+		System.out.println("introduce una secuencia de numeros");
+		System.out.println("introduce numero");
+		n=teclado.nextInt();
+		while(n!=0) {
+			
+			System.out.println("introduce numero");
+			n=teclado.nextInt();
+			
+		
+		
+			if(n%2==0) {
+		
+			System.out.println(n+n);
+			
+			
+			}
+		}
+		
+		
+		
+		
+		
+	
+		
+	}
+	
+}
